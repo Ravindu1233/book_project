229f9a4d9ce5ee5b8adb53996f3aefe3825750ba
Ravindu1233 <ravindu.dilshan1013@gmail.com>
2025-07-21 10:52:58 +0530

    Update Category

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Category;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        if(Auth::id()){
+
+            $user_type = Auth()->user()->usertype;
+
+            if($user_type == 'admin'){
+                
+                return view('admin.index');
+            }
+
+            else if ($user_type == 'user'){
+
+                return view ('home.index');
+            }
+        }
+
+        else
+        {
+            return redirect()->back();
+        }
+    }
+
+    public function category_page()
+    {
+        $data = Category::all();
+
+        return view('admin.category', compact('data'));
+    }
+
+public function add_category(Request $request)
+{
+    
+
+    $data = new Category;
+    $data->cat_title = $request->category;
+    $data->save();
+
+    return redirect()->back()->with('message','Category Added Successfully');
+}
+
+  public function cat_delete($id)
+  {
+    $data = Category::find($id);
+    $data->delete();
+    return redirect()->back()->with('message','category deleted successfully');
+  }
+
+  public function edit_category($id)
+  {
+
+    $data = Category::find($id);
+
+    return view('admin.edit_category', compact('data'));
+  }
+
+  public function update_category(Request $request, $id)
+  {
+    $data = Category::find($id);
+    $data->cat_title = $request->cat_name;
+    $data->save();
+    return redirect('/category_page')->with('message','Category Updated Successfully');
+  }
+
+  public function add_book()
+  {
+    return view('admin.add_book');
+  }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\AdminController;
+
+use App\Http\Controllers\HomeController;
+
+route::get('/', [HomeController::class,'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
+
+route::get('/home', [AdminController::class,'index']);
+
+route::get('/category_page', [AdminController::class,'category_page']);
+
+Route::post('/add_category', [AdminController::class, 'add_category']);
+
+route::get('/cat_delete/{id}', [AdminController::class,'cat_delete']);
+
+route::get('/edit_category/{id}', [AdminController::class,'edit_category']);
+
+route::post('/update_category/{id}',[AdminController::class,'update_category']);
+
+route::get('/add_book', [AdminController::class,'add_book']);
