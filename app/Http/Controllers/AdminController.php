f43dbf58787a8f12c5e963af1b2e8a763bf38cbb
Ravindu1233 <ravindu.dilshan1013@gmail.com>
2025-07-21 14:39:51 +0530

    Update books

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,9 +6,13 @@ use Illuminate\Http\Request;
 
 use App\Models\User;
 
+use App\Models\Book;
+
+
 use Illuminate\Support\Facades\Auth;
 
 use App\Models\Category;
+use PhpParser\Node\Expr\FuncCall;
 
 class AdminController extends Controller
 {
@@ -78,7 +82,126 @@ public function add_category(Request $request)
 
   public function add_book()
   {
-    return view('admin.add_book');
+    $data = Category::all();
+    return view('admin.add_book', compact('data'));
+  }
+
+  public function store_book(Request $request)
+  {
+    $data = new Book;
+
+    $data->title = $request->book_name;
+
+    $data->auther_name = $request->auther_name;
+
+    $data->price = $request->price;
+
+    $data->quantity = $request->quantity;
+
+    $data->description = $request->description;
+
+    $data->category_id = $request->category;
+
+    $book_image = $request->book_img;
+
+    $auther_image = $request->auther_img;
+     
+    if($book_image)
+    {
+        $book_image_name = time().'.'.$book_image->getClientOriginalExtension();
+
+        $request->book_img->move('book',$book_image_name);
+
+        $data->book_img = $book_image_name;
+    }
+
+     if($auther_image)
+    {
+        $auther_image_name = time().'.'.$book_image->getClientOriginalExtension();
+
+        $request->auther_img->move('auther',$auther_image_name);
+
+        $data->auther_img = $auther_image_name;
+    }
+
+    $data->save();
+
+     return redirect()->back()->with('success', 'Book added successfully!');
+
+
+
+  }
+
+  public function show_book()
+  {
+    $book = Book::all();
+
+    return view('admin.show_book', compact('book'));
+  }
+
+  public function book_delete($id)
+  {
+    $data = Book::find($id);
+
+    $data->delete();
+
+    return redirect()->back()->with('message','Book Deleted Successfully');
+  }
+
+  public function edit_book($id)
+  {
+    $data = Book::find($id);
+
+    $category = Category::all();
+
+    return view('admin.edit_book', compact('data','category'));
+  }
+
+  public function update_book(Request $request, $id)
+  {
+    $data = Book::find($id);
+
+     $data->title = $request->title;
+
+    $data->auther_name = $request->auther_name;
+
+    $data->price = $request->price;
+
+    $data->quantity = $request->quantity;
+
+    $data->description = $request->description;
+
+    $data->category_id = $request->category;
+
+    $book_image = $request->book_img;
+
+    $auther_image = $request->auther_img;
+     
+    if($book_image)
+    {
+        $book_image_name = time().'.'.$book_image->getClientOriginalExtension();
+
+        $request->book_img->move('book',$book_image_name);
+
+        $data->book_img = $book_image_name;
+    }
+
+     if($auther_image)
+    {
+        $auther_image_name = time().'.'.$book_image->getClientOriginalExtension();
+
+        $request->auther_img->move('auther',$auther_image_name);
+
+        $data->auther_img = $auther_image_name;
+    }
+
+      $data->save();
+
+      return redirect('/show_book')->with('message', 'Book Updated succesfully');
+
+
+
+
   }
 
 }
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -3,8 +3,8 @@
         <div class="sidebar-header d-flex align-items-center">
           <div class="avatar"><img src="admin/img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
           <div class="title">
-            <h1 class="h5">Mark Stephen</h1>
-            <p>Web Designer</p>
+            <h1 class="h5">Ravindu Dilshan</h1>
+            <p>Software engineer</p>
           </div>
         </div>
         <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
@@ -18,8 +18,8 @@
                   </a>
                   <ul id="exampledropdownDropdown" class="collapse list-unstyled">
                     <li><a href="{{url('add_book')}}">Add Books</a></li>
-                    <li><a href="#">Page</a></li>
-                    <li><a href="#">Page</a></li>
+                    <li><a href="{{url('show_book')}}">show Books</a></li>
+                    
                   </ul>
                 </li>
 
